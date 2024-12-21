2e9be968d6c2e99fafb47acfb7a61468729255f3
Sle3pingK1ng <108730069+Sle3pingK1ng@users.noreply.github.com>
2024-12-21 10:53:48 +0000

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,22 @@
+input("input 3 different numbers")
+userinput = ("12,23,33")
+sumtotal = sum(userinput)
+print(sumtotal)
+
+#first number minus minus second number
+firstnum = userinput[0]
+secondnum = userinput[1]
+result = firstnum - secondnum
+print(result)
+
+# multiply 
+thirdnum = userinput[-1]
+firstnum = userinput[0]
+result = thirdnum * firstnum
+print(result)
+
+# sum of 3 number multiply 
+sumtotal = sum(userinput)
+thirdnum = userinput[-1]
+sumdiv = sumtotal / thirdnum
+print(sumdiv)
